4071bb021f066f7eb6be4438a136d0cd4f8a9dc5
yunohu <119102587+NVN404@users.noreply.github.com>
2025-05-15 08:14:01 +0530

    Update lib.rs

--- tickr/programs/tickr/src/lib.rs
+++ tickr/programs/tickr/src/lib.rs
@@ -1,1 +1,53 @@
 
+#![allow(unexpected_cfgs)]
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("2FthqPAdr3DwQHH81T6P4cx5E15ynoipcmaZ8QBjoZ2i");
+
+#[program]
+pub mod tickr {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn setup_manager(ctx: Context<SetupManager>) -> Result<()> {
+        ctx.accounts.setup_manager(ctx.bumps.manager)
+    }
+
+    pub fn create_event(ctx: Context<CreateEvent>, args: CreateEventArgs) -> Result<()> {
+        ctx.accounts.create_event(args)
+    }
+
+    pub fn create_ticket(ctx: Context<CreateTicket>, args: CreateTicketArgs) -> Result<()> {
+        ctx.accounts.create_ticket(args)
+    }
+
+    pub fn withdraw_from_treasury(ctx: Context<WithdrawFromTreasury>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw_from_treasury(amount)
+    }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()
+    }
+
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()
+    }
+}
